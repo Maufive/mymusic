b03128a7fef30de90bb051b3c57b0c1d1461ae57
Niklas Albinsson <albinssonniklas@gmail.com>
2017-10-31 14:56:44 +0100

    second commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 import App from './components/App';
-import User from './components/User';
 import Login from './components/Login';
 
-import './index.css';
+import './scss/index.scss';
+import './scss/asdf.css';
 
 class Root extends React.Component {
   render() {
@@ -15,7 +15,6 @@ class Root extends React.Component {
         <div>
           <Route path="/" component={App} />
           <Route exact path="/" component={Login} />
-          <Route path="/user/:accessToken/:refreshToken" component={User} />
         </div>
       </Router>
     );
