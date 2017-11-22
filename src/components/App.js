40dedd69323a80c91a1e5b97a47379a8d840a8e9
Niklas Albinsson <albinssonniklas@gmail.com>
2017-11-22 16:44:18 +0100

    ..

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: 'tjenalaeget'
+      username: ''
     };
     this.searchUser = this.searchUser.bind(this);
   }
